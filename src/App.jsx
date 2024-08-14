5cdfa82c4cbd0c8c4d4e47834c704b981032d9e8
Harish.A <asianpirate27@gmail.com>
2024-08-14 11:21:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Userid } from './components/Fett';
+
+function App() {
+  return (
+    <div className="container">
+      <Userid />
+    </div>
+  );
+}
+
+export default App;
